e0c7e2f874ccc60eee7aabf38e6ac2922e5e6aad
Daniel Kaiser <dskaiser82@gmail.com>
2020-05-07 21:25:59 -0700

    css for fonts

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Home.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+
 
 class Home extends React.Component{
   constructor(props){
@@ -18,7 +21,13 @@ class Home extends React.Component{
   render(){
     return(
       <div className="home">  
-        <img className="page" src="/wolf/img/p1.png"/>      
+        <img className="page" src="/wolf/img/p1.png"/>  
+        <div className="awe">
+          <p>In The beginning there was a spoirit wolf</p>
+          <p><FontAwesomeIcon icon={faArrowLeft}/></p>
+
+        </div>
+      
       </div>
     )
   }
