b326de19f5b923224abaf8c70d2712e121c2055e
Daniel Kaiser <dskaiser82@gmail.com>
2020-05-07 20:18:57 -0700

    removed routes

--- src/router/router.js
+++ src/router/router.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import App from '../App';
-import About from '../pages/About/About-class.js';
-import Contact from '../pages/Contact/Contact.js';
-import AllPoke from '../pages/AllPoke/AllPoke.js';
-import ContextEx from '../pages/ContextEx/ContextEx.js';
 import { Route } from 'react-router-dom'
 
 function myRoutes() {
     return( 
      <div>   
         <Route path="/" exact component={App} /> 
-        <Route path="/home" exact component={App} />
-        <Route path="/about" exact component={About} />
-        <Route path="/contact" exact component={Contact} />
-        <Route path="/poke" exact component={AllPoke} />
-        <Route path="/denny" exact component={App} />
-        <Route path="/context" exact component={ContextEx} />
     </div>
     );
   }
