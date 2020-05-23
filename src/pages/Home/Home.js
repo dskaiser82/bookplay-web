f7296ed4db28f23f5b91164db689a13ed8e24427
Daniel Kaiser <dskaiser82@gmail.com>
2020-05-22 19:59:30 -0700

    added correct text for the book pages

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -18,67 +18,67 @@ class Home extends React.Component{
         {
           img: "/wolf/img/pages/p1.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'In The beginning there was a Spirit Wolf, and here Mother was the Moon'
         },
         {
           img: "/wolf/img/pages/p2.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'The Moon showered her daughter with the love of moonlight.'
         },
         {
           img: "/wolf/img/pages/p3.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'As the first night ended, Spirit Wolf took her very first sleep.'
         },
         {
           img: "/wolf/img/pages/p4.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'As Spirit Wolf awoke, she did not see her Mother above.  She looked, but could not see her Mother Moon. "My Mother has left me", said Spirit Wolf.'
         },
         {
           img: "/wolf/img/pages/p5.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: '"But I will find her!" And Spirit Wolf began her journey for the day. '
         },
         {
           img: "/wolf/img/pages/p6.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'Spirit Wolf had great speed and traveled far.'
         },
         {
           img: "/wolf/img/pages/p7.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'She ventured into a tall forest. And the Old Oak said, "My Mother is the Sun and her love as warm as the summer."'
         },
         {
           img: "/wolf/img/pages/p8.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'Spirit Wolf ran until land ended and waves began. And the Sea Turtle said, "My Mother is the Sea and her love is as vast as eternity." '
         },
         {
           img: "/wolf/img/pages/p9.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'Spirit Wolf journeyed to a land of ice. And the Proud Penguin said, "My Mother is the Snow, and her love is as soft as a blanket."  '
         },
         {
           img: "/wolf/img/pages/p10.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'Spirit Wolf climbed the highest mountain. The Great Eagle said, "My Mother is the Sky, and her love is as strong as the wind."'
         },
         {
           img: "/wolf/img/pages/p11.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'And then looking to the sky above, Spirit Wolf saw a faint image of Mother Moon. "She sleeps", said the Great Eagle. "But she will awaken come nighttime"'
         },
         {
           img: "/wolf/img/pages/p12.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'And as the Sun set and day turned into night, Mother Moon awoke.'
         },
         {
           img: "/wolf/img/pages/p13.png" ,
           vo: null,
-          text: "In The beginning there was a Spirit Wolf, and here Mother was the Moon"
+          text: 'In the beauty of moonlight the daughter wolf declared, "I am Spirit Wolf.  My Mother is the Moon. Her love is as bright as diamond." And Spirit Wolf cried, "Howwwoooooo."'
         }
       ] 
     }
