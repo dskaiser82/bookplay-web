aec2f08ab8d6f5d134869028349dbb205101e609
Daniel Kaiser <dskaiser82@gmail.com>
2021-04-03 19:11:51 -0700

    adjusting wolf box css

--- src/pages/Home/Start/TextBox/index.js
+++ src/pages/Home/Start/TextBox/index.js
@@ -3,12 +3,7 @@ import React from 'react';
 import './styles.scss';
 
 const textBox = ({ children }) => {
-  return (
-    <div className="wolf_box.png">
-      <img src="/wolf/img/wolf_box.png" />
-      {children}
-    </div>
-  );
+  return <div className="wolf-box">{children}</div>;
 };
 
 export default textBox;
