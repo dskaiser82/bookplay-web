612d1434f7951b4d4ff1d3810cb08893df3a5d71
Dan Kaiser <dskaiser82@gmail.com>
2023-04-08 12:53:17 -0700

    convert class to function

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,82 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.scss';
 import Page from './Start/Page';
 import { pages } from './pages';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Home() {
+  const [pageCounter, setPageCounter] = useState(0);
+  const voRef = React.createRef();
 
-    this.voRef = React.createRef();
+  const playAudio = (file: any) => {
+    voRef.current.pause();
+    voRef.current.load();
+    voRef.current.play();
+  };
 
-    this.pageUp = this.pageUp.bind(this);
-
-    this.state = {
-      menu: {
-        menu: 'end-titles', //end-titles or start-options
-        options: {
-          on: false,
-          music: true,
-          vo: true,
-        },
-        img: {
-          cover: '/wolf/img/menu/cover.png',
-          titles: '/wolf/img/menu/titles.png',
-          credits: '/wolf/img/menu/credits.png',
-        },
-      },
-      pageCounter: 0,
-      pages: pages,
-    };
-  } //end c
-
-  playAudio(file: any) {
-    this.voRef.current.pause();
-    this.voRef.current.load();
-    this.voRef.current.play();
-  }
-
-  playMusic() {
+  const playMusic = () => {
     let music = new Audio('/wolf/music/wolf_flute_final.mp3');
     music.play();
-  }
+  };
 
-  playPageTurn() {
+  const playPageTurn = () => {
     const audio = new Audio('/shared/audio/page_turn.mp3');
     audio.play();
-  }
+  };
 
-  pageUp() {
-    if (this.state.pageCounter === 0) {
-      this.playMusic();
+  const pageUp = () => {
+    if (pageCounter === 0) {
+      playMusic();
     }
 
-    this.setState({ pageCounter: this.state.pageCounter + 1 });
+    setPageCounter(pageCounter + 1);
 
-    this.playPageTurn();
+    playPageTurn();
 
-    this.playAudio();
-  }
+    playAudio();
+  };
 
-  render() {
-    let pages = this.state.pages;
-    let pageCounter = this.state.pageCounter;
-    return (
-      <div className="home">
-        <audio ref={this.voRef} preload="true">
-          <source
-            src={
-              pageCounter <= pages.length - 1
-                ? pages[pageCounter]['vo']
-                : ''
-            }
-          />
-        </audio>
+  console.log('pageCounter', pageCounter);
 
-        <Page pageUp={this.pageUp} page={pages[pageCounter]} />
-      </div>
-    );
-  }
+  console.log();
+
+  return (
+    <div className="home">
+      <audio ref={voRef} preload="true">
+        <source
+          src={
+            pageCounter <= pages.length - 1
+              ? pages[pageCounter]['vo']
+              : ''
+          }
+        />
+      </audio>
+
+      <Page pageUp={pageUp} page={pages[pageCounter]} />
+    </div>
+  );
 }
-
-export default Home;
