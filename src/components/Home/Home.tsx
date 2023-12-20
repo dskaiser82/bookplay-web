0a2b941c247f238a28fb2515c0e8ed104c9e9dea
dskaiser82 <dskaiser82@gmail.com>
2023-12-19 20:01:50 -0800

    Add toggle music

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,20 +6,31 @@ import { debounce } from '../../helpers';
 
 export default function Home() {
   const [pageCounter, setPageCounter] = useState(0);
-  const voRef = useRef();
+  const voRef = useRef<HTMLAudioElement>(null);
+  const musicRef = useRef(new Audio('/wolf/music/wolf_flute_final.mp3')); // Create the Audio object here
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
 
   const playAudio = () => {
-    voRef.current.pause();
-    voRef.current.load();
-    voRef.current.play();
+    if (voRef.current) {
+      voRef.current.pause();
+      voRef.current.load();
+      voRef.current.play().catch(error => {
+        console.error("Playback failed", error);
+      });
+    }
   };
 
-  const playMusic = () => {
-    let music = new Audio('/wolf/music/wolf_flute_final.mp3');
-    music.volume = 0.3;
-    music.play();
+  const toggleMusicPlayback = () => {
+    if (isMusicPlaying) {
+      musicRef.current.pause();
+    } else {
+      musicRef.current.play().catch(error => {
+        console.error("Playback failed", error);
+      });
+    }
+    setIsMusicPlaying(!isMusicPlaying);
   };
-
+  
   const playPageTurn = () => {
     const audio = new Audio('/shared/audio/page_turn.mp3');
     audio.play();
@@ -33,36 +44,42 @@ export default function Home() {
   const debouncedPageUp = debounce(function () {
     pageUp();
   }, 500);
-
+  
   useEffect(() => {
-    if (pageCounter === 0) {
-      document.addEventListener('keydown', debouncedPageUp);
-    }
-
+    document.addEventListener('keydown', debouncedPageUp);
     return () => {
       document.removeEventListener('keydown', debouncedPageUp);
     };
-  }, []);
+  }, [pageCounter, debouncedPageUp]);
 
   useEffect(() => {
     playAudio();
     if (pageCounter > 13) {
       setPageCounter(0);
     }
-
-    if(pageCounter === 1) {
-      playMusic();
-    }
-
   }, [pageCounter]);
+
+  useEffect(() => {
+    // Set the volume and other properties of the music here
+    musicRef.current.volume = 0.3;
+
+    // Cleanup function to pause and reset the music when the component unmounts
+    return () => {
+      musicRef.current.pause();
+      musicRef.current.currentTime = 0; // Optionally reset the time
+    };
+  }, []);
 
   return (
     <div className="home">
-      <audio ref={voRef} preload="true">
-        <source src={pages[pageCounter]?.vo || ''} />
+      <audio ref={voRef} preload="auto">
+        <source src={pages[pageCounter]?.vo || ''} type="audio/mpeg" />
       </audio>
 
       <Page page={pages[pageCounter]} />
+      <button className="button-play" onClick={toggleMusicPlayback}>
+        {isMusicPlaying ? 'Pause Music' : 'Play Music'}
+      </button>
     </div>
   );
 }
