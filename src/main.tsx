cf7e7e4548646097d74359bc08ffcac3886b557f
Daniel Kaiser <dskaiser82@gmail.com>
2024-03-31 13:20:49 -0700

    import by client

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // main.tsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import myRoutes from './router/router'; // Import your routes
 
