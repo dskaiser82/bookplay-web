5816388d35350df427bfe6e018d42a613edd0935
Daniel Kaiser <dskaiser82@gmail.com>
2024-03-31 11:04:13 -0700

    Add pages for beauty and the beast

---
+++ src/components/Story/Beast/pages.ts
@@ -0,0 +1,149 @@
+export const pages = [
+  {
+    img: '/public/beast/img/pages/0.png',
+  },
+  {
+    img: '/public/beast/img/pages/1.png',
+  },
+  {
+    img: '/public/beast/img/pages/2.png',
+  },
+  {
+    img: '/public/beast/img/pages/3.png',
+  },
+  {
+    img: '/public/beast/img/pages/4.png',
+  },
+  {
+    img: '/public/beast/img/pages/5.png',
+  },
+  {
+    img: '/public/beast/img/pages/6.png',
+  },
+  {
+    img: '/public/beast/img/pages/7.png',
+  },
+  {
+    img: '/public/beast/img/pages/8.png',
+  },
+  {
+    img: '/public/beast/img/pages/9.png',
+  },
+  {
+    img: '/public/beast/img/pages/10.png',
+  },
+  {
+    img: '/public/beast/img/pages/11.png',
+  },
+  {
+    img: '/public/beast/img/pages/12.png',
+  },
+  {
+    img: '/public/beast/img/pages/13.png',
+  },
+  {
+    img: '/public/beast/img/pages/14.png',
+  },
+  {
+    img: '/public/beast/img/pages/15.png',
+  },
+  {
+    img: '/public/beast/img/pages/16.png',
+  },
+  {
+    img: '/public/beast/img/pages/17.png',
+  },
+  {
+    img: '/public/beast/img/pages/18.png',
+  },
+  {
+    img: '/public/beast/img/pages/19.png',
+  },
+  {
+    img: '/public/beast/img/pages/20.png',
+  },
+  {
+    img: '/public/beast/img/pages/21.png',
+  },
+  {
+    img: '/public/beast/img/pages/22.png',
+  },
+  {
+    img: '/public/beast/img/pages/23.png',
+  },
+  {
+    img: '/public/beast/img/pages/24.png',
+  },
+  {
+    img: '/public/beast/img/pages/25.png',
+  },
+  {
+    img: '/public/beast/img/pages/26.png',
+  },
+  {
+    img: '/public/beast/img/pages/27.png',
+  },
+  {
+    img: '/public/beast/img/pages/28.png',
+  },
+  {
+    img: '/public/beast/img/pages/29.png',
+  },
+  {
+    img: '/public/beast/img/pages/30.png',
+  },
+  {
+    img: '/public/beast/img/pages/31.png',
+  },
+  {
+    img: '/public/beast/img/pages/32.png',
+  },
+  {
+    img: '/public/beast/img/pages/33.png',
+  },
+  {
+    img: '/public/beast/img/pages/34.png',
+  },
+  {
+    img: '/public/beast/img/pages/35.png',
+  },
+  {
+    img: '/public/beast/img/pages/36.png',
+  },
+  {
+    img: '/public/beast/img/pages/37.png',
+  },
+  {
+    img: '/public/beast/img/pages/38.png',
+  },
+  {
+    img: '/public/beast/img/pages/39.png',
+  },
+  {
+    img: '/public/beast/img/pages/40.png',
+  },
+  {
+    img: '/public/beast/img/pages/41.png',
+  },
+  {
+    img: '/public/beast/img/pages/42.png',
+  },
+  {
+    img: '/public/beast/img/pages/43.png',
+  },
+  {
+    img: '/public/beast/img/pages/44.png',
+  },
+  {
+    img: '/public/beast/img/pages/45.png',
+  },
+  {
+    img: '/public/beast/img/pages/46.png',
+  },
+  {
+    img: '/public/beast/img/pages/47.png',
+  },
+  {
+    img: '/public/beast/img/pages/48.png',
+  },
+];
